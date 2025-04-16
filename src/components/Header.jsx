0680f2258fca926b2d145c44e4e8f7a531abe471
Ahmed Mamdoh <akremmamdoh@gmail.com>
2025-04-16 19:36:31 +0200

    Header and Menu Done

---
+++ src/components/Header.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom"; // Add NavLink import
+import logo from "../assets/logo.svg";
+import btata from "../assets/btata.png";
+import search from "../assets/search.svg";
+
+const Header = () => {
+  return (
+    <div className="navbar bg-orange-light ">
+      <div className="container py-2 ">
+        <div className="navbar-start flex gap-4">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/">Menu</Link>
+              </li>
+              <li>
+                <Link to="/">Offers</Link>
+              </li>
+              <li>
+                <Link to="/">Service</Link>
+              </li>
+              <li>
+                <Link to="/">About Us</Link>
+              </li>
+            </ul>
+          </div>
+          <Link to="/">
+            <img src={logo} alt="" />
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="flex gap-9 px-1 text-grey-dark font-semibold text-body-md">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-black"
+                    : "hover:text-black transition-all duration-300 ease-in-out"
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/menu"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-black"
+                    : "hover:text-black transition-all duration-300 ease-in-out"
+                }
+              >
+                Menu
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/offers"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-black"
+                    : "hover:text-black transition-all duration-300 ease-in-out"
+                }
+              >
+                Offers
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/Service"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-black"
+                    : "hover:text-black transition-all duration-300 ease-in-out"
+                }
+              >
+                Service
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/AboutUs"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-black"
+                    : "hover:text-black transition-all duration-300 ease-in-out"
+                }
+              >
+                About Us
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end flex gap-4">
+          <button className="bg-orange-dark p-[15px] rounded-full">
+            <img src={search} alt="" />
+          </button>
+          <img src={btata} alt="" className="w-[50px] h-[50px] rounded-full" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/MenuProduct.jsx
@@ -0,0 +1,58 @@
+import { Rating } from "@mui/material";
+import React from "react";
+
+const MenuProduct = ({ img, name, rating, numOfRating, price }) => {
+  const [numOfOrders, setNumOfOrders] = React.useState(0);
+  const addOrder = () => {
+    setNumOfOrders((order) => order + 1);
+  };
+  const removeOrder = () => {
+    if (numOfOrders > 0) {
+      setNumOfOrders((order) => order - 1);
+    }
+  };
+
+  return (
+    <div
+      className="flex flex-col justify-center items-center
+    gap-5 py-5 px-4 bg-orange-light
+    border-dashed border-4 border-orange-mid rounded-b-[2000px] rounded-t-full
+    transition-all
+    hover:scale-105
+    duration-300 ease-in-out"
+    >
+      <img src={img} alt="product" className="w-[250px] h-[250px]" />
+      <p className="text-body-lg font-bold w-3/4 text-center">{name}</p>
+      <div className="flex justify-center items-center gap-1">
+        <Rating
+          name="half-rating-read"
+          defaultValue={rating}
+          precision={0.25}
+          readOnly
+        />
+        <p className="text-grey-dark text-body-lg">({numOfRating})</p>
+      </div>
+      <p className="text-heading-sm">₹{price}</p>
+      <div className="flex items-center justify-center  gap-9 text-body-lg">
+        <button
+          className="bg-orange-dark rounded-full w-12 h-12 text-white"
+          onClick={removeOrder}
+        >
+          -
+        </button>
+        <span className="text-body-lg font-semibold">{numOfOrders}</span>
+        <button
+          className="bg-orange-dark rounded-full w-12 h-12 text-white"
+          onClick={addOrder}
+        >
+          +
+        </button>
+      </div>
+      <button className="px-9 py-4 bg-orange-dark rounded-full font-semibold text-white ">
+        ADD TO CART
+      </button>
+    </div>
+  );
+};
+
+export default MenuProduct;
