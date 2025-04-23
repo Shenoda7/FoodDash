d42a18836b6d83cca5aca738c9a57522d1bdf06f
Ahmed Mamdoh <akremmamdoh@gmail.com>
2025-04-24 00:43:01 +0200

    feat: add cart functionality and API integration
    - Added axios
    
    - Created formatter utility for currency formatting
    
    - Integrated menu data fetching from API in App component
    
    - Implemented cart state management across components
    
    - Added dynamic cart item rendering and total calculation

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,40 @@ import NotFound from "./pages/NotFound.jsx";
 import Footer from "./components/Footer.jsx";
 import Service from "./pages/Service.jsx";
 import Offers from "./pages/Offers.jsx";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [menu, setMenu] = useState([]);
+  const [cart, setCart] = useState([]);
+  function getMenu() {
+    const url = import.meta.env.VITE_API_URL;
+    axios({ url: `${url}/products`, method: "GET" })
+      .then((res) => {
+        setMenu(res.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching menu:", error);
+      });
+  }
+  useEffect(() => {
+    getMenu();
+  }, []);
   return (
     <>
       <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
+          <Route
+            path="/menu"
+            element={<Menu menu={menu} cart={cart} setCart={setCart} />}
+          />
           <Route path="/about" element={<About />} />
-          <Route path="/cart" element={<Cart />} />
+          <Route
+            path="/cart"
+            element={<Cart cart={cart} setCart={setCart} />}
+          />
           <Route path="/service" element={<Service />} />
           <Route path="/offers" element={<Offers />} />
           <Route path="*" element={<NotFound />} />
--- src/pages/Cart/CartItem.jsx
+++ src/pages/Cart/CartItem.jsx
@@ -1,15 +1,41 @@
 import React, { useState } from "react";
 import { FaRegTrashAlt } from "react-icons/fa";
-
-const CartItem = ({ img, name, price }) => {
-  const [quantity, setQuantity] = useState(1);
+import { formatUSD } from "../../util/formatter";
+const CartItem = ({ dishData, img, setCart }) => {
+  const [quantity, setQuantity] = useState(dishData.quantity);
   const addOrder = () => {
     setQuantity((order) => order + 1);
+    setCart((cart) => {
+      return cart.map((item) =>
+        item.id === dishData.id
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      );
+    });
   };
   const removeOrder = () => {
-    if (quantity > 0) {
+    if (quantity > 1) {
       setQuantity((order) => order - 1);
+      setCart((cart) => {
+        return cart.map((item) =>
+          item.id === dishData.id
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        );
+      });
     }
+
+    if (quantity === 0) {
+      setCart((cart) => {
+        return cart.filter((item) => item.id !== dishData.id);
+      });
+    }
+  };
+
+  const removeFromCart = () => {
+    setCart((cart) => {
+      return cart.filter((item) => item.id !== dishData.id);
+    });
   };
   return (
     <div
@@ -17,7 +43,9 @@ const CartItem = ({ img, name, price }) => {
     shadow-lg flex-col sm:flex-row gap-3 sm:gap-0"
     >
       <img src={img} alt="" className="w-28 h-28 sm:w-20 sm:h-20" />
-      <p className="text-body-md sm:text-body-sm font-semibold">{name}</p>
+      <p className="text-body-md sm:text-body-sm font-semibold">
+        {dishData.name}
+      </p>
       <div className="flex items-center w-full sm:w-1/3 justify-around">
         <button
           className="bg-orange-dark rounded-full w-12 h-12 text-white text-body-lg"
@@ -33,8 +61,10 @@ const CartItem = ({ img, name, price }) => {
           +
         </button>
       </div>
-      <p className="text-body-lg font-medium">₹{price * quantity}</p>
-      <button>
+      <p className="text-body-lg font-medium">
+        {formatUSD(dishData.price * quantity)}
+      </p>
+      <button onClick={removeFromCart}>
         <FaRegTrashAlt className="w-6 h-6" />
       </button>
     </div>
